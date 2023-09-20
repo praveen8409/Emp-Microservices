dc179de186bc5ee5c4bba1f95554795e97dd93b7
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-20 21:07:53 +0530

    Create department service with basic setup

---
+++ department-registry/src/test/java/com/msd/departmentregistry/DepartmentRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msd.departmentregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
